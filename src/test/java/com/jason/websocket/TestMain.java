e7ce8b7b0b5da8aa1855a4a70ea2f093c45bcc5a
Yusik <yusik.koh@mytrum.com>
2018-09-21 18:17:49 +0900

    java8 Interface dafault

--- src/test/java/com/jason/websocket/TestMain.java
+++ src/test/java/com/jason/websocket/TestMain.java
@@ -1,14 +1,19 @@
 package com.jason.websocket;
 
+import org.junit.Test;
+import org.springframework.boot.test.context.TestComponent;
+
+@TestComponent
 public class TestMain {
+
+    @Test
+    public void binaryTest() {
     
-    public static void main(String[] args) {
-    
-//        int mask = 0b11100101111011101111111111010000;
+        //        int mask = 0b11100101111011101111111111010000;
         int mask =    0b11100101;
         int encoded = 0b11010100;
         int decoded = 0b00110001;
-        
+    
         decoded = mask ^ encoded;
     
         System.out.println(mask);
@@ -17,6 +22,12 @@ public class TestMain {
         System.out.println(0b10000001 - 128);
     
         System.out.println(229 ^ 212);
+        
+    }
+    
+    @Test
+    public void test() {
+    
     }
     
 }
